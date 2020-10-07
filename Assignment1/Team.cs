182ae51eabbde506115ae311df77a3f0b66a8610
sholden8340 <25555894+Sholden8340@users.noreply.github.com>
2020-10-07 23:26:25 +0200

    Add project files.

---
+++ Assignment1/Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Team
+    {
+
+        public List<Programmer> Programmers { get; private set; }
+        public Team()
+        {
+            Programmers = new List<Programmer>
+            {
+                new Programmer("Gred", Speciality.PHP),
+                new Programmer("Andy", Speciality.Csharp),
+                new Programmer("Jackson", Speciality.HTML),
+                new Programmer("Horseraddish", Speciality.Java)
+            };
+        }
+
+        public void AddProgammer(Programmer p)
+        {
+            Programmers.Add(p);
+        }
+
+        public override String ToString()
+        {
+            String team = null;
+            foreach (Programmer p in Programmers)
+            {
+                team += p.ToString();
+            }
+            return team;
+        }
+
+        public void PrintAllTeamMembers()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+    }
+}
---
+++ Assignment2/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Dice
+    {
+        public int Value { get; set; }
+
+        private Random rng;
+
+        public Dice(Random rng)
+        {
+            this.rng = rng;
+        }
+
+
+        public void Throw()
+        {
+            Value = rng.Next(1, 7);
+        }
+
+        public void DisplayValue()
+        {
+            Console.Write(" " + Value);
+        }
+
+        public int Getvalue()
+        {
+            return Value;
+        }
+
+        public void SetValue(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+
+}
